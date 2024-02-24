d13de5e86c6e4c9f3191ca83b0ef21299b24b5ef
shimura233 <2763221922@qq.com>
2024-02-24 15:05:34 +0800

    fix implementation of improved lazyDijkstraSSSP(the previous one is actually eager dijkstra)

--- src/main/MSTandShortestPath/SSSP/LazyDijkstraSSSP2.java
+++ src/main/MSTandShortestPath/SSSP/LazyDijkstraSSSP2.java
@@ -3,10 +3,11 @@ package main.MSTandShortestPath.SSSP;
 import edu.princeton.cs.algs4.DirectedEdge;
 import edu.princeton.cs.algs4.EdgeWeightedDigraph;
 import kotlin.Pair;
-import main.PriorityQueue.IndexMinPQ;
+
+import java.util.PriorityQueue;
 
 /**
- * only work for graph without negative edges. modified from {@link LazyDijkstraSSSP}
+ * only work for graph without negative edges. (modified from {@link LazyDijkstraSSSP})
  */
 public class LazyDijkstraSSSP2 extends SSSP {
 
@@ -23,32 +24,53 @@ public class LazyDijkstraSSSP2 extends SSSP {
             dist[i] = Double.POSITIVE_INFINITY;
         }
         dist[s] = 0;
-        IndexMinPQ<Double> pq = new IndexMinPQ<>(g.V());
-        pq.insert(s, dist[s]);
+        PriorityQueue<DirectedEdge> pq = new PriorityQueue<>((o1, o2) -> {
+            double diff = dist[o1.from()] + o1.weight() - (dist[o2.from()] + o2.weight());
+            if (diff < 0) {
+                return -1;
+            } else {
+                if (diff > 0) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+        });
         boolean[] marked = new boolean[g.V()];
-        int u;
-        while ((u = pq.deleteMin()) != -1) {
+        marked[s] = true;
+        relax(s, pq);
+        while (!pq.isEmpty()) {
+            DirectedEdge edge = pq.poll();
+            int u = edge.to();
             if (!marked[u]) {
                 marked[u] = true;
-                for (DirectedEdge e:g.adj(u)) {
-                    double newDist = dist[u] + e.weight();
-                    int to = e.to();
-                    if (newDist < dist[to]) {
-                        dist[to] = newDist;
-                        edgeTo[to] = e;
-                        if (pq.contains(to)) {
-                            pq.decreaseKey(to, dist[to]);
-                        } else {
-                            pq.insert(to, dist[to]);
-                        }
-                    }
-                }
+                relax(u, pq);
+            }
+        }
+    }
+
+    private void relax(int u, PriorityQueue<DirectedEdge> pq) {
+        for (DirectedEdge e:g.adj(u)) {
+            int to = e.to();
+            double newDist = dist[u] + e.weight();
+            if (newDist < dist[to]) {
+                // `to` must have not been marked - because there is no negative edge.
+                // for those edges e the `to` of which not marked but that can't construct a shorter path to `to`
+                // there is no need to insert it. Because there must have been an edge e' in pq s.t. the to of which is the same
+                // as e and dist[e`.from] + weight <= dist[e.from] + weight. e` will be polled out before e and `to` will be marked.
+                // then e will be ignored.
+                dist[to] = newDist;
+                edgeTo[to] = e;
+                // because there is no negative edges, dist[u = e.from()] won't change after e is inserted into pq.
+                // pq's comparator is ensured to perform normally.
+                pq.add(e);
             }
         }
     }
 
     public static void main(String[] args) {
-        Pair<EdgeWeightedDigraph, Integer> param = parseGraph(args);
-        unitTest(new LazyDijkstraSSSP2(param.getFirst(), param.getSecond()), param.getFirst(), param.getSecond());
+        Pair<EdgeWeightedDigraph, Integer> params = parseGraph(args);
+        unitTest(new LazyDijkstraSSSP2(params.getFirst(), params.getSecond()), params.getFirst(), params.getSecond());
     }
 }
+
